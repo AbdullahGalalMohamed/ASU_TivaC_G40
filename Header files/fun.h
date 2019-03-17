f0ec01247ac8f116dd062804341d262c11f64ab8
AbdullahGalalMohamed <36282511+AbdullahGalalMohamed@users.noreply.github.com>
2019-03-17 23:20:49 +0200

    New function with description

--- Header files/fun.h
+++ Header files/fun.h
@@ -2,11 +2,15 @@
 #include "tm4c123gh6pm.h"
 #define GPIO_LOCK_KEY           0x4C4F434B  // Unlocks the GPIO_CR register
 #define GPIO_PABCD_M						0xFF		    // mask for enable port A,B,C,D
-#define GPIO_E_M								0x3F		    // mask for enable port E
+#define GPIO_PA_M               0xF0			
+#define GPIO_E_M								0x1C		    // mask for enable port E
 #define GPIO_F_M								0x1F		    // mask for enable port F
-#define GPIO_PCTL_PA3210_M		(GPIO_PCTL_PA3_M | GPIO_PCTL_PA2_M| GPIO_PCTL_PA1_M | GPIO_PCTL_PA0_M)
-#define GPIO_PCTL_PB3210_M		(GPIO_PCTL_PB3_M | GPIO_PCTL_PB2_M| GPIO_PCTL_PB1_M | GPIO_PCTL_PB0_M)
+#define GPIO_PCTL_PA7654_M		(GPIO_PCTL_PA7_M | GPIO_PCTL_PA6_M)
+#define GPIO_PCTL_PB3210_M		(GPIO_PCTL_PB6_M| GPIO_PCTL_PB5_M | GPIO_PCTL_PB4_M|GPIO_PCTL_PB3_M | GPIO_PCTL_PB2_M| GPIO_PCTL_PB1_M | GPIO_PCTL_PB0_M)
 #define GPIO_PCTL_PD3210_M		(GPIO_PCTL_PD3_M | GPIO_PCTL_PD2_M| GPIO_PCTL_PD1_M | GPIO_PCTL_PD0_M)
+#define GPIO_PCTL_PC654_M			(GPIO_PCTL_PC6_M| GPIO_PCTL_PC5_M | GPIO_PCTL_PC4_M)
+#define GPIO_PCTL_PE12345_M		(GPIO_PCTL_PE4_M | GPIO_PCTL_PE4_M | GPIO_PCTL_PE3_M | GPIO_PCTL_PE2_M | GPIO_PCTL_PE1_M)
+
 
 
 /*******************************************************************************
@@ -14,7 +18,7 @@
  *******************************************************************************/
 
 /*
-Initialize port(A,B,D) by enabling
+Initialize port(A,B,C,D,E) by enabling
 the clock, unlocking the port, and making the selected mode
 digital and change the direction of the selected pins to output
 */
@@ -23,7 +27,11 @@ void PortA_Init(void);
 
 void PortB_Init(void);
 
-void PortD_Init(void);
+void PortC_Init();
+
+void PortD_Init();
+
+void PortE_Init();
 
 /*
 Connect sw2 to internal pull-down resistor "Reset button"
@@ -45,17 +53,28 @@ Call initialization functions for switches and ports
 */
 void SystemInit(void);
 /*
-Reset the counter (Data of port A & port B & port D)
+Reset the counter
 */
 void reset(void);
 
 /*
-Increment the counter (Data of port A & port B & port D)
+Increment the counter;
 */
 void Increment(void);
 
 /*
-Decrement the counter (Data of port A & port B & port D)
+Decrement the counter
 */
 
 void Decrement(void);
+
+/* Split the counter to 3 digits and display them on the 7-segments
+
+*/
+void Decoder (int count ,int index);
+//Delay
+
+
+void delay_milli(int n);
+
+
