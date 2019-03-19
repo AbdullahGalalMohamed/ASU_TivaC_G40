bcb15afd961d46fd66ce8df48060ea2347b6f2cd
AbdullahGalalMohamed <36282511+AbdullahGalalMohamed@users.noreply.github.com>
2019-03-19 23:39:50 +0200

    Update Decoder.c

--- C files/Decoder.c
+++ C files/Decoder.c
@@ -1,4 +1,6 @@
 # include "fun.h"
+
+
 void Decoder (int count ,int index)
 {
 	switch (count) 
@@ -80,6 +82,93 @@ void Decoder (int count ,int index)
 		{
 		GPIOA->DATA = 0x40;
 		GPIOE->DATA = 0x0C;
+			
 		}
 		break;
 		}
+		case 5:
+		{	
+		if (index==1)
+			GPIOB->DATA = 0x12;
+		else if (index ==2)
+		{
+		GPIOC->DATA = 0x10;
+		GPIOD->DATA = 0x02;
+		}
+		else if (index ==3)
+		{ 
+			GPIOE->DATA = 0x08;
+			GPIOA->DATA = 0x80;
+		
+		}
+		break;
+		}
+		case 6:
+		{	
+		if (index==1)
+			GPIOB->DATA = 0x02;
+		else if (index ==2)
+		{
+		GPIOC->DATA = 0x00;
+		GPIOD->DATA = 0x02;
+		}
+		else if (index ==3)
+		{
+			GPIOE->DATA = 0x00;
+			GPIOA->DATA= 0x80;
+			
+		}
+		break;
+		}
+		case 7:
+		{	
+		if (index==1)
+			GPIOB->DATA = 0x78;
+		else if (index ==2)
+		{
+		GPIOC->DATA = 0x70;
+		GPIOD->DATA = 0x08;
+		}
+		else if (index ==3)
+		{		GPIOE->DATA = 0x3C;
+			  GPIOA->DATA = 0x0;
+			
+		}
+		break;
+		}
+		case 8:
+		{	
+		if (index==1)
+			GPIOB->DATA = 0x00;
+		else if (index ==2)
+		{
+		GPIOC->DATA = 0x00;
+		GPIOD->DATA = 0x00;
+		}
+		else if (index ==3)
+		{
+			GPIOE->DATA = 0x00;
+			GPIOA->DATA=0x00;
+			
+		}
+		break;
+		}
+		case 9:
+		{	
+		if (index==1)
+			GPIOB->DATA = 0x10;
+		else if (index ==2)
+		{
+		GPIOC->DATA = 0x10;
+		GPIOD->DATA = 0x00;
+		}
+		else if (index ==3)
+		{GPIOE->DATA = 0x08;
+		 GPIOA->DATA=0x00;
+			
+		}
+		break;
+		}	
+	}		
+}
+
