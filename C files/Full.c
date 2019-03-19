2a47ee1769071799e7d8e9d04af833d7ade9e1bc
Abdelhady Ahmed <31725200+HadyP@users.noreply.github.com>
2019-03-19 23:46:26 +0200

    Update Full.c

--- C files/Full.c
+++ C files/Full.c
@@ -1,1 +1,44 @@
+#include "fun.h"
+int count=0;
+void Increment()
+{
+	int flag=0;
+  int count2;
+	int digit;
+	//count = GPIOA->DATA + (GPIOB->DATA << 1) ;
+	if(count==999)
+	{
+		count=0;
+	}
+	
+	else{
+		++(count);
+		
+	}
+	count2=count;
+	  while(count2 >= 0 && flag < 4) //do till num greater than  0
+    {   
+        digit = (count2) % 10;
+			  
+
+			  flag++;
+        count2 = count2 / 10;
+        if (flag==1)
+				{
+					Decoder (digit , flag);
+					//GPIOA->DATA=((GPIOA->DATA)&0xF0) |(digit);
+				}	
+        else if (flag==2)
+				{
+					Decoder (digit , flag);
+					//GPIOB->DATA=(GPIOB->DATA& 0xF0) |(digit);
+				}	
+      else if (flag==3)
+				{
+					Decoder (digit , flag);
+					//GPIOD->DATA=(GPIOD->DATA& 0xF0 ) |(digit);
+				}					
+    }
+		
+}
 
