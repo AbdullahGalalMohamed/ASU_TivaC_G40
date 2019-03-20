41522121180ffe9ab083b4a8a6788ad094025529
saraAbdelkhalek <36415283+saraAbdelkhalek@users.noreply.github.com>
2019-03-20 15:16:51 +0200

    Update Init_Switch.c

--- LCD/C files/Init_Switch.c
+++ LCD/C files/Init_Switch.c
@@ -1,25 +1,29 @@
 #include "fun.h"
 
 
-
-void SW1_Init(){
+void LCD_init(void){
 	
-	SYSCTL->RCGCGPIO |= 0x01;  // enable mode clk for port A
-	while ((SYSCTL->RCGCGPIO & 0x01) == 0) {} ; //delay
-	GPIOA->LOCK=GPIO_LOCK_KEY;
-  GPIO_PORTA_CR_R |= 0x40;		
-	GPIOA->AMSEL&= ~0x40U;
-	GPIOA->PCTL&= ~GPIO_PCTL_PA6_M;
-	GPIOA->AFSEL &= ~0x40U;
-	GPIOA->DIR&= ~0x40U;
-	GPIOA->PUR|= 0x40U;
-	GPIOA->DEN |= 0x40U;	
-				
+SYSCTL->RCGCGPIO |= 0x02; //PORTB clock
+SYSCTL->RCGCGPIO |= 0x10;//PORTE clock
+GPIOE->DIR |=0x1C; //PORTE controls RS , E and  R/W // 2 , 3 , 4
+GPIOE->DEN |=0x1C;
+GPIOB->DIR |=0xFF; //PORTB D0-D7
+GPIOB->DEN |=0xFF; //PORTB D0-D7
+LCD_Cmd(0x38); //8-bits,2 display lines, 5x7 font
+LCD_Cmd(0x06); //increments automatically
+LCD_Cmd(0x0F); //Turn on display
+LCD_Cmd(0x01); //clear display
+ 
 }
-void SW0_Init(){
-	
-	SYSCTL->RCGCGPIO |= 0x01;  
-	while ((SYSCTL->RCGCGPIO & 0x01) == 0) {} ; //delay
-	GPIOA->LOCK=GPIO_LOCK_KEY;
-  GPIO_PORTA_CR_R |= 0x80;		
-	GPIOA->AMSEL&= ~0x80U;
+ 
+void LCD_Cmd(unsigned char command)
+{
+
+GPIOE->DATA =0x00; //RS=0, E=0,RW=0
+GPIOB->DATA =command;
+GPIOE->DATA |= 0x08;
+delay_micro(0);
+GPIOE->DATA = 0x00;
+delay_micro(2000); 
+if(command <4) delay_milli(2); else delay_micro(37); 
+}
