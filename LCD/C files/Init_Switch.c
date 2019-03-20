3a5517745e0a5e3dd6896eecb29b4a1d8a4651d4
Abdelhady Ahmed <31725200+HadyP@users.noreply.github.com>
2019-03-20 15:28:17 +0200

    Update Init_Switch.c

--- LCD/C files/Init_Switch.c
+++ LCD/C files/Init_Switch.c
@@ -16,3 +16,35 @@ void SW1_Init(){
 	GPIOA->DEN |= 0x40U;	
 				
 }
+void SW0_Init(){
+	
+	SYSCTL->RCGCGPIO |= 0x01;  
+	while ((SYSCTL->RCGCGPIO & 0x01) == 0) {} ; //delay
+	GPIOA->LOCK=GPIO_LOCK_KEY;
+  GPIO_PORTA_CR_R |= 0x80;		
+	GPIOA->AMSEL&= ~0x80U;
+	GPIOA->PCTL&= ~GPIO_PCTL_PA7_M;
+	GPIOA->AFSEL &= ~0x80U;
+	GPIOA->DIR&= ~0x80U;
+	GPIOA->PUR|= 0x80U;
+	GPIOA->DEN |= 0x80U;	
+				
+}
+
+
+void SW2_Init(){
+	
+	SYSCTL->RCGCGPIO |= 0x08;  
+	while ((SYSCTL->RCGCGPIO & 0x08) == 0) {} ; //delay
+	GPIOD->LOCK=GPIO_LOCK_KEY;
+  GPIO_PORTD_CR_R |= 0x04;		
+	GPIOD->AMSEL&= ~0x04U;
+	GPIOD->PCTL&= ~GPIO_PCTL_PD2_M;
+	GPIOD->AFSEL &= ~0x04U;
+	GPIOD->DIR&= ~0x04U;
+	GPIOD->PUR|= 0x04U;
+	GPIOD->DEN |= 0x04U;
+		
+				
+}
+
