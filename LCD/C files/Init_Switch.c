a951721ed38667d729bc362dcb0d5490e5b7b392
Shrouk Ragheb Amer <36502437+ShroukRagheb@users.noreply.github.com>
2019-03-20 15:25:55 +0200

    Create Init_Switch.c

---
+++ LCD/C files/Init_Switch.c
@@ -0,0 +1,18 @@
+#include "fun.h"
+
+
+
+void SW1_Init(){
+	
+	SYSCTL->RCGCGPIO |= 0x01;  // enable mode clk for port A
+	while ((SYSCTL->RCGCGPIO & 0x01) == 0) {} ; //delay
+	GPIOA->LOCK=GPIO_LOCK_KEY;
+  GPIO_PORTA_CR_R |= 0x40;		
+	GPIOA->AMSEL&= ~0x40U;
+	GPIOA->PCTL&= ~GPIO_PCTL_PA6_M;
+	GPIOA->AFSEL &= ~0x40U;
+	GPIOA->DIR&= ~0x40U;
+	GPIOA->PUR|= 0x40U;
+	GPIOA->DEN |= 0x40U;	
+				
+}
